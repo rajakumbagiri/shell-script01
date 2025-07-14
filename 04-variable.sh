e4b513f8f878396523b1a41dbf88eafca868379c
rajakumbagiri <kumbagiri.raja75.com>
2025-07-14 20:39:26 +0530

    code save

---
+++ 04-variable.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1::Hello $PERSON2 , how are you?"
+echo "$PERSON2::Hello $PERSAON1, I am fine . what aboubt you?"
+echo "$PERSON1::i am fine too, how is your work"
+echo "$PERSON2::NOt bad , I am thinking to upgrade to devops"
