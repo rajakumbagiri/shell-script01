f8bbdd0b13e3d0684a10b4cf8c2bc91f85ee8e2e
rajakumbagiri <kumbagiri.raja75.com>
2025-07-14 21:02:33 +0530

    Data types practice

--- 05-data-types.sh
+++ 05-data-types.sh
@@ -3,5 +3,5 @@
 MOVIES=("RRR" "DJTILLU" "MURARI")
 
 echo "First movie is :${MOVIES[0]}"
-echo "Second movie is :${MOVIE[1]}"
+echo "Second movie is :${MOVIES[1]}"
 echo "Third movie is :${MOVIES[@]}"
