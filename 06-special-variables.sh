f1acc413717728ab6e69ed4ff6d4e347f60e180e
rajakumbagiri <kumbagiri.raja75.com>
2025-07-16 16:06:34 +0530

    Data types practice

---
+++ 06-special-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "All variable: $@"
+echo "Number of variables passed: $#"
+echo "Script name: $0"
+echo "current working directory: $PWD"
+echo "Which user is running this script: $USER"
+echo "Home directory of current user: $HOME"
+echo "Host name: $HOSTNAME"
+echo "Process ID of current Shell script: $$"
+sleep 60
