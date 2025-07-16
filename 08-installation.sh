b8a9d6fe35e04edf44719fc4940878acc0309521
rajakumbagiri <kumbagiri.raja75.com>
2025-07-16 16:47:30 +0530

    installation process

--- 08-installation.sh
+++ 08-installation.sh
@@ -5,10 +5,25 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then
     echo "please run this script with root access."
+    exit1
 
 else
     echo "you are super user."
 fi
 
-dnf install mysql -y
-echo "is script proceeding"
+dnf install -y
+if [ $? -n 0 ]
+   echo "Installation of MySQL...FAILURE"
+else 
+   echo "Installation of MySQL...SUCCESS"
+fi
+
+dnf install git -y
+if[ $? -n 0 ]
+then 
+    echo "Installation of Git...FAILURE"
+else
+    echo "Installation Git...SUCCESS"
+fi
+
+echo "is cript is proceeding"
